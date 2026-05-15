ffe5ba0a1ed1280d78aeb7153d506fdc08c067d9
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-05-15 15:12:00 +0530

    Solved Remove Duplicate Letters

--- LeetCode/Medium/Remove Duplicate Letters/solution.java
+++ LeetCode/Medium/Remove Duplicate Letters/solution.java
@@ -1,31 +1,29 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        Stack<Character> stack = new Stack<>();
-        int[] freq = new int[26];
-        boolean[] visited = new boolean[26];
-        // count frequency
-        for (char ch : s.toCharArray()) {
+        Stack<Character> stack=new Stack<>();
+        int freq[]=new int[26];
+        boolean[] visited=new boolean[26];
+          for (char ch : s.toCharArray()) {
             freq[ch - 'a']++;
         }
-
-        for (char ch : s.toCharArray()) {
-            // current character used once
-            freq[ch - 'a']--;
-            // already in stack
-            if (visited[ch - 'a'])
+        for(char ch:s.toCharArray()){
+            if(visited[ch-'a']) {
+                freq[ch-'a']--;
                 continue;
-            while (!stack.isEmpty()&& ch < stack.peek() && freq[stack.peek() - 'a'] > 0) {
-                visited[stack.pop() - 'a'] = false;
             }
-
+            while(!stack.isEmpty() && ch<stack.peek() && freq[stack.peek()-'a']>1){
+                freq[stack.peek()-'a']-=1;
+                visited[stack.pop()-'a']=false;
+            }
             stack.push(ch);
-            visited[ch - 'a'] = true;
+            visited[ch-'a']=true;
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : stack)
-            sb.append(c);
-        return sb.toString();
+        StringBuilder result=new StringBuilder();
+        for(char ch:stack)
+        {
+            result.append(ch);
+        }
+        return result.toString();
     }
+
 }
