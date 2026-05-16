3d1e19b61b702703bd53bcf89f4abee74ae8484a
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-05-16 12:33:26 +0530

    Solved Subtree of Another Tree

---
+++ LeetCode/Easy/Subtree of Another Tree/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean issametree(TreeNode p,TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null ||q==null) return false;
+        return p.val==q.val && issametree(p.left,q.left) && issametree(p.right,q.right);
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null && subRoot==null) return true;
+        if(root==null || subRoot==null) return false;
+        return issametree(root,subRoot) || isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+
+    }
+}
