f7ae2bc8e91d8610e6642a4d5d186c5c4fe3d7d5
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-07-16 10:05:40 +0530

    Solved Jump Game

--- LeetCode/Medium/Jump Game/solution.java
+++ LeetCode/Medium/Jump Game/solution.java
@@ -1,1 +1,11 @@
-CODE_EXTRACTION_FAILED
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJump=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxJump) return false;
+            maxJump=Math.max(maxJump,i+nums[i]);
+            if(maxJump>nums.length-1)return true;
+        }
+        return true;
+    }
+}
