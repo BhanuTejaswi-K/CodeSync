7fa2124ca0217d001e6fc8784f8044586dcf3a4e
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-07-28 15:23:01 +0530

    Solved Minimum Falling Path Sum II

--- LeetCode/Hard/Minimum Falling Path Sum II/solution.java
+++ LeetCode/Hard/Minimum Falling Path Sum II/solution.java
@@ -1,24 +1,31 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-        int n=grid.length;
-        int dp[][]=new int[n][n];
-
-        for(int i=0;i<n;i++) dp[0][i]=grid[0][i];
-
+       int n=grid.length;
+       int prev[]=new int[n];
+       for(int j=0;j<n;j++) prev[j]=grid[0][j];
         for(int i=1;i<n;i++){
+            int min1=Integer.MAX_VALUE;
+            int min2=Integer.MAX_VALUE;
+            int mincol=-1;
             for(int j=0;j<n;j++){
-                int min=Integer.MAX_VALUE;
-                for(int k=0;k<n;k++){
-                    if(k!=j) min=Math.min(min,dp[i-1][k]);
+                 if (prev[j] < min1) {
+                    min2 = min1;
+                    min1 = prev[j];
+                    mincol = j;
+                } else if (prev[j] < min2) {
+                    min2 = prev[j];
+                }
             }
-            dp[i][j]=min+grid[i][j];
+            int curr[]=new int[n];
+            for(int j=0;j<n;j++){
+                if(j==mincol)curr[j]=grid[i][j]+min2;
+                else curr[j]=grid[i][j]+min1;
+            }
+            prev=curr;
         }
-        }
-        int ans = Integer.MAX_VALUE;
-        for (int j = 0; j < n; j++) {
-            ans = Math.min(ans, dp[n - 1][j]);
-        }
-
+        int ans=Integer.MAX_VALUE;
+        for(int i:prev) ans=Math.min(ans,i);
         return ans;
+
     }
 }
