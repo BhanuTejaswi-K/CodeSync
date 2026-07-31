da008bb495adc92838e41d25f264a512fe78ab82
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-07-31 10:05:08 +0530

    Solved Longest Increasing Subsequence

---
+++ LeetCode/Medium/Longest Increasing Subsequence/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[][]=new int[n+1][n+1];
+
+        for(int index=n-1;index>=0;index--){
+            for(int prev=index-1;prev>=-1;prev--){
+                int notpick=dp[index+1][prev+1];
+                int pick=0;
+                if(prev==-1||nums[index]>nums[prev]){
+                    pick=1+dp[index+1][index+1];
+                }
+                dp[index][prev+1]=Math.max(pick,notpick);
+            }
+        }
+        return dp[0][0];
+    }
+}
