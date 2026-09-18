febfd81eb7aac89c3cf59aadbcf24a9011459c72
Bhanu Tejaswi <bhanutejaswichowdary@gmail.com>
2026-09-18 23:36:45 +0530

    Solved String to Integer (atoi)

--- LeetCode/Medium/String to Integer (atoi)/solution.java
+++ LeetCode/Medium/String to Integer (atoi)/solution.java
@@ -8,14 +8,13 @@ class Solution {
         if(s.charAt(0)=='-') sign=-1;
         int MAX=Integer.MAX_VALUE,MIN=Integer.MIN_VALUE;
         int i=(s.charAt(0)=='+' || s.charAt(0)=='-')?1:0;
-        while(i < s.length()) {
-            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i)-'0';
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
+        while(i<s.length()){
+            if(s.charAt(i)== ' '||!Character.isDigit(s.charAt(i))) break;
+            ans=ans*10+s.charAt(i)-'0';
+            if(sign==-1 && -1*ans<MIN) return MIN;
+            if(sign== 1 && ans>MAX)return MAX;   
             i++;
-        }
-        
+        }        
         return (int)(sign*ans);
     }
+}
